0192a65728822c958b94d0fae78a1420faffc055
Gaurav Deep <gauravdeep8660@gmail.com>
2025-07-02 00:49:25 +0530

    Non Lambda

--- internal/db/jobUtils.go
+++ internal/db/jobUtils.go
@@ -10,68 +10,66 @@ import (
 	"go.mongodb.org/mongo-driver/bson/primitive"
 )
 
-var (
-	GetAllJobs = func(cont context.Context) ([]model.Job, error) {
-		ctx, cancel := context.WithTimeout(cont, 5*time.Second)
-		defer cancel()
+func GetAllJobs(cont context.Context) ([]model.Job, error) {
+	ctx, cancel := context.WithTimeout(cont, 5*time.Second)
+	defer cancel()
 
-		collection := GetCollection(JobCollection)
+	collection := GetCollection(JobCollection)
 
-		cursor, err := collection.Find(ctx, bson.M{})
-		if err != nil {
-			return nil, err
-		}
-		defer cursor.Close(ctx)
+	cursor, err := collection.Find(ctx, bson.M{})
+	if err != nil {
+		return nil, err
+	}
+	defer cursor.Close(ctx)
 
-		var jobs []model.Job
-		if err := cursor.All(ctx, &jobs); err != nil {
-			return nil, err
-		}
-
-		return jobs, nil
+	var jobs []model.Job
+	if err := cursor.All(ctx, &jobs); err != nil {
+		return nil, err
 	}
 
-	AddJob = func(cont context.Context, job model.Job) error {
-		ctx, cancel := context.WithTimeout(cont, 5*time.Second)
-		defer cancel()
+	return jobs, nil
+}
 
-		collection := GetCollection(JobCollection)
+func AddJob(cont context.Context, job model.Job) error {
+	ctx, cancel := context.WithTimeout(cont, 5*time.Second)
+	defer cancel()
 
-		_, err := collection.InsertOne(ctx, job)
-		return err
+	collection := GetCollection(JobCollection)
+
+	_, err := collection.InsertOne(ctx, job)
+	return err
+}
+
+func GetJobByID(cont context.Context, id string) (model.Job, error) {
+	ctx, cancel := context.WithTimeout(cont, 5*time.Second)
+	defer cancel()
+
+	objID, err := primitive.ObjectIDFromHex(id)
+	if err != nil {
+		return model.Job{}, errors.New("invalid job ID format")
 	}
 
-	GetJobByID = func(cont context.Context, id string) (model.Job, error) {
-		ctx, cancel := context.WithTimeout(cont, 5*time.Second)
-		defer cancel()
+	collection := GetCollection(JobCollection)
 
-		objID, err := primitive.ObjectIDFromHex(id)
-		if err != nil {
-			return model.Job{}, errors.New("invalid job ID format")
-		}
-
-		collection := GetCollection(JobCollection)
-
-		var job model.Job
-		if err := collection.FindOne(ctx, bson.M{"_id": objID}).Decode(&job); err != nil {
-			return model.Job{}, err
-		}
-
-		return job, nil
+	var job model.Job
+	if err := collection.FindOne(ctx, bson.M{"_id": objID}).Decode(&job); err != nil {
+		return model.Job{}, err
 	}
 
-	DeleteJob = func(cont context.Context, id string) error {
-		ctx, cancel := context.WithTimeout(cont, 5*time.Second)
-		defer cancel()
+	return job, nil
+}
 
-		objID, err := primitive.ObjectIDFromHex(id)
-		if err != nil {
-			return errors.New("invalid job ID format")
-		}
+func DeleteJob(cont context.Context, id string) error {
+	ctx, cancel := context.WithTimeout(cont, 5*time.Second)
+	defer cancel()
 
-		collection := GetCollection(JobCollection)
-
-		_, err = collection.DeleteOne(ctx, bson.M{"_id": objID})
-		return err
+	objID, err := primitive.ObjectIDFromHex(id)
+	if err != nil {
+		return errors.New("invalid job ID format")
 	}
-)
+
+	collection := GetCollection(JobCollection)
+
+	_, err = collection.DeleteOne(ctx, bson.M{"_id": objID})
+	return err
+}
